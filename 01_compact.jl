8010b5e41a3da16a2d9d5596e3285da2bc85e5cb
tpoisot <timothee.poisot@umontreal.ca>
2024-10-11 16:53:48 -0400

    (visible confusion)

--- 01_compact.jl
+++ 01_compact.jl
@@ -1,19 +1,35 @@
 using JSON3
 
-all_int = unique(vcat(JSON3.read("Virus.json"), JSON3.read("Viruses.json")))
+# What we need to read
+relevant_files = readdir()
+filter!(startswith("Virus"), relevant_files)
+filter!(endswith(".json"), relevant_files)
+
+# Read everything
+compendium = unique(vcat(JSON3.read.(relevant_files)...))
+
+# Function to unmess the mess
+function foreheadkiss(record)
+    o_source = record["source_taxon_name"]
+    o_target = record["target_taxon_name"]
+    relation = record["interaction_type"]
+    bor_source = record["source_specimen_basis_of_record"]
+    bor_target = record["target_specimen_basis_of_record"]
+    authority = record["study_title"]
+    return Dict(
+        :from => o_source,
+        :to => o_target,
+        :as => relation,
+        :bors => bor_source,
+        :bort => bor_target,
+        :ref => authority
+    )
+end
+
+allint = unique(foreheadkiss.(compendium))
 
 if ~isfile("ObiWanVirobi.json")
     open("ObiWanVirobi.json", "w") do io
-        JSON3.pretty(io, all_int)
-    end
-end
-
-# Simplified version
-
-simplified = unique([Dict(:from => r.source_taxon_name, :as => r.interaction_type, :to => r.target_taxon_name) for r in all_int])
-
-if ~isfile("compact.json")
-    open("compact.json", "w") do io
-        JSON3.pretty(io, simplified)
+        JSON3.pretty(io, allint)
     end
 end
